4de28872ea048bafdbf3f9360488592e489d5b49
Sheetal <sheetalbhardwaj@gmail.com>
2025-05-02 11:57:17 +0530

    change

--- 15-Backtracking/printPath.java
+++ 15-Backtracking/printPath.java
@@ -1,8 +1,8 @@
 public class printPath {
     public static void main(String[] args) {
         int rows = 3;
-        int cols = 3;
-        print(1, 1, rows, cols, "");
+        int cols = 4;
+        print(0, 0, rows-1, cols-1, "");
     }
 
     private static void print(int sr, int sc, int er, int ec, String s) {
@@ -17,3 +17,24 @@ public class printPath {
         print(sr, sc + 1, er, ec, s + "R"); // right
     }
 }
+
+
+// public class printPath {
+//     public static void main(String[] args) {
+//         int rows = 3;
+//         int cols = 3;
+//         print(1, 1, rows, cols, "");
+//     }
+
+//     private static void print(int sr, int sc, int er, int ec, String s) {
+//         if (sr > er || sc > ec)
+//             return; // out of bounds
+//         if (sr == er && sc == ec) { // base case
+//             System.out.println(s);
+//             return;
+//         }
+//         // Recursive calls
+//         print(sr + 1, sc, er, ec, s + "D"); // down
+//         print(sr, sc + 1, er, ec, s + "R"); // right
+//     }
+// }
